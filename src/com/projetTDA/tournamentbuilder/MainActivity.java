e918f1ee5f92afb9aa72a28b22c1dcc4adee03da
stanley <stan974@msn.com>
2014-04-05 01:34:20 +0200

    Création de structure de l'application : 	page principal avec 3 boutons 	structure des pages : 		statistique 		gérer joueur 		gérer tournoi

--- src/com/projetTDA/tournamentbuilder/MainActivity.java
+++ src/com/projetTDA/tournamentbuilder/MainActivity.java
@@ -1,5 +1,6 @@
 package com.projetTDA.tournamentbuilder;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -7,63 +8,49 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 //	private TextView texte = null;
 	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-//		texte = new TextView(this);
-//		texte.setText("Bonjour trou de balle!!");
-//		setContentView(texte);
-
-		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
+	public void onCreate(Bundle savedInstanceState) {
+		  super.onCreate(savedInstanceState);
+		  setContentView(R.layout.activity_main);
+		  
+		  final Button gererTournoi = (Button) findViewById(R.id.gererTournoiButton);
+		  gererTournoi.setOnClickListener(new OnClickListener() {
+					
+		  @Override
+		  public void onClick(View v) {
+			Intent intent = new Intent(MainActivity.this, GererTournoi.class);
+			startActivity(intent);
+			}
+		});
+		  
+		  
+		  final Button gererJoueur = (Button) findViewById(R.id.gererJoueurButton);
+		  gererJoueur.setOnClickListener(new OnClickListener() {
+					
+		  @Override
+		  public void onClick(View v) {
+			Intent intent = new Intent(MainActivity.this, GererJoueur.class);
+			startActivity(intent);
+			}
+		});
+		  
+		  final Button statistique = (Button) findViewById(R.id.statistiqueButton);
+		  statistique.setOnClickListener(new OnClickListener() {
+					
+		  @Override
+		  public void onClick(View v) {
+			Intent intent = new Intent(MainActivity.this, Statistique.class);
+			startActivity(intent);
+			}
+		}); 
+		  
+		  
+		   }
 
 }
